687325cf73bc600a6c7d1888df27a32a3e67e558
Camille Wintz <camille.wintz@lweb-education.net>
2018-01-29 19:42:04 +0100

    show filler images when actor images are missing

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -5,17 +5,31 @@ import { Actor } from '../game/ActorsCollection';
 import { Conf } from '../game/Conf';
 
 export class Question extends React.Component<{ onYes: () => void, onNo: () => void, actor: Actor, movie: Movie }>{
+    actorImage(){
+        if(this.props.actor && this.props.actor.file_path){
+            return <img src={ `${Conf.imagesPath}${this.props.actor.file_path}?api_key=${Conf.apiKey}` } />;
+        }
+        return <img src={ 'https://blackvelvetcoat.files.wordpress.com/2012/01/yak.jpg' } />
+    }
+
+    movieImage(){
+        if(this.props.movie && this.props.movie.poster_path){
+            return <img src={ `${Conf.imagesPath}${this.props.movie.poster_path}?api_key=${Conf.apiKey}` } />;
+        }
+        return <img src={ 'https://blackvelvetcoat.files.wordpress.com/2012/01/yak.jpg' } />
+    }
+    
     render(){
         return (
             <div className={ "question" }>
                 <h2>Cet acteur a-t-il joué dans ce film ?</h2>
                 <div className={ "movie" }>
                     <h2>{ this.props.movie.title }</h2>
-                    <img src={ `${Conf.imagesPath}${this.props.movie.poster_path}?api_key=${Conf.apiKey}` } />
+                    { this.movieImage() }
                 </div>
                 <div className={ "actor "}>
                     <h2>{ this.props.actor.name }</h2>
-                    <img src={ `${Conf.imagesPath}${this.props.actor.file_path}?api_key=${Conf.apiKey}` } />
+                    { this.actorImage() }
                 </div>
                 <div className={ "options" }>
                     <button onClick={ () => this.props.onYes() }>Oui</button>
